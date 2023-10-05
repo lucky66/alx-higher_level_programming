6350ce58a6116aa3c2cd6f795da1896caeafd3eb
lucky66 <luckychukwu66@gmail.com>
2023-10-05 16:47:52 -0700

    Import modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    sum = 0
+    if len(argv) > 1:
+        for x in range(1, len(argv)):
+            sum += int(argv[x])
+    print(sum)
