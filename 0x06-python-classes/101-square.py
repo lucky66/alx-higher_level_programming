b3769c6921862316d27bb40ad0a1adb4bcecbf9a
lucky66 <luckychukwu66@gmail.com>
2023-10-24 15:25:44 -0700

    python classes

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,56 +1,109 @@
 #!/usr/bin/python3
-"""a class Square that defines a square"""
+""" Module 101-square. """
+"""
+Defines class Square with private size & position; and public area & my_print
+Can access and update size and position
+Can print to stdout the square using #'s
+"""
 
 
 class Square:
-    """Square class"""
+    """
+    class Square definition
+    Args:
+        size (int): size of a side in square
+    Functions:
+        __init__(self, size, position)
+        size(self)
+        size(self, value)
+        position(self)
+        position(self, value)
+        area(self)
+        my_print(self)
+    """
+
     def __init__(self, size=0, position=(0, 0)):
-        """initialization"""
-        if type(size) != int:
-            raise TypeError("must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        """
+        Initializes square
+        Attributes:
+            size (int): defaults to 0 if none; don't use __size to call setter.
+            position (int): tuple of two positive integers.
+        """
+        self.size = size
         self.position = position
 
     @property
     def size(self):
+        """"
+        Getter
+        Return: size
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("must be an integer")
-        if value < 0:
+        """
+        Setter
+        Args:
+            value: sets size to value if int and >= 0
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     @property
     def position(self):
+        """"
+        Getter
+        Return: position.
+        """
         return self.__position
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2:
+        """
+        Setter
+        Args:
+            value: sets position to tuple if value is tuple of 2 positive ints.
+        """
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or type(value[1]) is not int or \
+           value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(isinstance(x, int) for x in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(i >= 0 for i in value):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        else:
+            self.__position = value
 
     def area(self):
-        return self.__size * self.__size
+        """
+        Calculates area of square.
+        Returns:
+            area.
+        """
+        return (self.__size)**2
 
     def my_print(self):
-        siz = self.__size
-        pos = self.__position
-        if siz == 0:
-            print()
+        if self.__size == 0:
+            print("")
         else:
-            if pos[1] > 0:
-                print()
-            for i in range(siz):
-                [print(" ", end="") for x in range(pos[0])]
-                [print("#", end="") for y in range(siz)]
-                print()
+            print("\n" * self.__position[1], end="")
+            print("\n".join([" " * self.__position[0] +
+                             "#" * self.__size
+                             for rows in range(self.__size)]))
+
+    def __str__(self):
+        """
+        String representation of square so call to print works.
+        Example: print(my_square).
+        """
+        string = ""
+        if self.__size == 0:
+            return string
+
+        string += "\n" * self.position[1]
+        string += "\n".join([" " * self.__position[0] +
+                             "#" * self.__size
+                             for rows in range(self.__size)])
+        return string
